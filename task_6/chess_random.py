720e0cac7342a5d1b409f4d1eaccdae71553e3da
Laskist <tatiana.zhdanova92@mail.ru>
2023-06-16 19:55:03 +0300

    practical

--- task_6/chess_random.py
+++ task_6/chess_random.py
@@ -8,21 +8,17 @@ from typing import Tuple
 def func_random(x: list, y: list, try_count: int) -> tuple[list, list]:
     num = 8
     count = 1
-    result = True
+    print(x, y)
     while count <= try_count:
+        print(count)
         for i in range(num):
             for j in range(i + 1, num):
                 if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
-                    result = False
-        if result:
-            count += 1
-            print(count)
-            print(x, y)
-            shuffle(y)
-            break
-        else:
-            shuffle(y)
-            print(y)
-            continue
+                    break
+            else:
+                count += 1
+                print(count)
+                print(x, y)
+                shuffle(y)
 
     return None
