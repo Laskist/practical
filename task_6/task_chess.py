a8416425a5fb3ab3428f55247c7f652f08b3e74d
Laskist <tatiana.zhdanova92@mail.ru>
2023-06-18 11:33:25 +0300

    practical

--- task_6/task_chess.py
+++ task_6/task_chess.py
@@ -1,8 +1,8 @@
 import random
 from hw_module import chess_random as chs2
-from hw_module import chess_q as chs
+# from hw_module import chess_q as chs
 if __name__ == "__main__":
 
     n = 8
-    print(chs.queen(*([int(i) for i in input().split()] for i in range(n))))
-    chs2.func_random(x=[int(i+1) for i in range(n)], y=random.sample(range(1, 9), 8), try_count=4)
+    # print(chs.queen(*([int(i) for i in input().split()] for i in range(n))))
+    chs2.func_random(x=[int(i+1) for i in range(n)], y=random.sample(range(1, n+1), n), try_count=4)
