bbef76e98d9b4892456753de1727401dc6aa02cb
Laskist <tatiana.zhdanova92@mail.ru>
2023-07-03 23:27:46 +0300

    practical11

--- task_11/str_time.py
+++ task_11/str_time.py
@@ -4,15 +4,23 @@
 # (time.time)
 import time
 
+
 class MySrting(str):
     """expand class str."""
-
     def __new__(cls, value: str, name: str):
         """expand metod new params value and name"""
         instance = super().__new__(cls, value)
+        instance.value = value
         instance.name = name
         instance.time = time.time()
         return instance
+
+    def __repr__(self):
+        return f'MyString {self.name}, {self.time}, {self.value}'
+
+    def __str__(self):
+        return f' {self.name}, {self.time}, {self.value}'
+
 
 if __name__ == '__main__':
     mystr = MySrting("чтото НЕПОНЯТНОЕ", "текст")
@@ -20,3 +28,5 @@ if __name__ == '__main__':
     print(mystr.upper())
     help(MySrting)
     help(mystr)
+    print(mystr.__repr__())
+    print(mystr)
